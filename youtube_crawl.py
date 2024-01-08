b6c8548f5d96ab6ce228959a9065ffe95059c352
ZZHbible <804459116@qq.com>
2024-01-08 21:13:21 +0800

    避免重复下载视频

--- youtube_crawl.py
+++ youtube_crawl.py
@@ -6,14 +6,26 @@ import json
 import os
 import subprocess
 from tqdm import tqdm
-
 from common import retry
+import yt_dlp
+import glob
+from typing import  List
 
+def get_youtube_title(url):
+    ydl_opts = {
+        'quiet': True,
+        'no_warnings': True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        return info_dict.get('title', None)
 
 @retry(3)
-def crawl_video(video_path: str):
-    command = f'yt-dlp -f 22 --output "{output_dir}/%(title)s.%(ext)s" {video_path}'
-    subprocess.run(command, shell=True, check=True)
+def crawl_video(video_path: str,exist_mp4:List):
+    title = get_youtube_title(video_path)
+    if title not in exist_mp4:
+        command = f'yt-dlp -f 22 --output "{output_dir}/{title}/%(title)s.%(ext)s" {video_path}'
+        subprocess.run(command,shell=True,check=True)
 
 
 if __name__ == '__main__':
@@ -23,5 +35,7 @@ if __name__ == '__main__':
     with open('config/total_video.json', 'r') as f:
         video_list = json.load(f)
 
+    exist_mp4 = glob.glob(output_dir+'/*')
+
     for video_path in tqdm(video_list, desc="正在获取视频"):
-        crawl_video(video_path)
+        crawl_video(video_path,exist_mp4)
