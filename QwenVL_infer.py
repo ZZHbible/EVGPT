8dff5c6a5b8367e053a28754b58c0ddff79545ea
ZZHbible <804459116@qq.com>
2024-01-10 20:07:04 +0800

    增加QwenVL多图推理脚本

--- QwenVL_infer.py
+++ QwenVL_infer.py
@@ -2,3 +2,24 @@
 # author = 'ZZH'
 # time = 2024/1/10
 # project = QwenVL_infer
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.generation import GenerationConfig
+
+# 如果您希望结果可复现，可以设置随机数种子。
+# torch.manual_seed(1234)
+
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
+
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat", device_map="cuda", trust_remote_code=True).eval()
+model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
+
+query = tokenizer.from_list_format([
+    {'image': 'assets/mm_tutorial/Chongqing.jpeg'},
+    {'image': 'assets/mm_tutorial/Beijing.jpeg'},
+    {'text': '上面两张图片分别是哪两个城市？请对它们进行对比。'},
+])
+torch.manual_seed(5678)
+response, history = model.chat(tokenizer, query=query, history=None)
+print(response)
